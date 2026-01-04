27f5d0fc9d74c6f9a822d6b6d630fd42ab596542
a <annigeriabhishek3@gmail.com>
2026-01-04 18:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ st.title("🩺 Diabetes Prediction App")
 preg = st.number_input("Pregnancies", 0, 20)
 glucose = st.number_input("Glucose Level", 0, 200)
 bp = st.number_input("Blood Pressure", 0, 150)
+skin = st.number_input("Skin Thickness", 0, 100)
+insulin = st.number_input("Insulin", 0, 900)
+bmi = st.number_input("BMI", 0.0, 70.0)
+dpf = st.number_input("Diabetes Pedigree Function", 0.0, 3.0)
+age = st.number_input("Age", 1, 100)
 
 input_data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
 
@@ -18,4 +23,5 @@ if st.button("Predict"):
         st.error("❌ Diabetic")
     else:
         st.success("✅ Not Diabetic")
+
 
